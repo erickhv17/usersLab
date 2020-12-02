1f949981f5ece90bd4123778ebacbb59ddf826e7
Herrera Erick (SO/OPM1.4-Hda) <hee5hda@bosch.com>
2020-12-02 03:57:23 -0600

    Hot fix, link and title

--- students-app/src/components/navBar.js
+++ students-app/src/components/navBar.js
@@ -13,12 +13,12 @@ class NavBar extends Component {
   render() {
     return (
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Students-App</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="https://www.google.co.cr/">About us</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
